079ad3e94e74188ce2b4580e600fd5f17e8a82b5
SamithaN <samithaneranjan23@gmail.com>
2022-02-08 16:54:30 +0530

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApiCRUD.Interface;
+using RestApiCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUD.Controllers
+{
+    
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private IEmployeeData _employeeData;
+        public EmployeesController(IEmployeeData employeeData)
+        {
+            _employeeData = employeeData;
+        }
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployees()
+        {
+            return Ok(_employeeData.GetEmployees());
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetEmployee(Guid id)
+        {
+            var employee = _employeeData.GetEmployee(id);
+            if(employee != null)
+            {
+                return Ok(_employeeData.GetEmployee(id));
+            }
+            return NotFound($"Employee with ID: {id} was not found");
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployee(Employee employee)
+        {
+            _employeeData.AddEmployee(employee);
+
+            return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path
+                + "/" + employee.Id, employee);
+            
+        }
+
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = _employeeData.GetEmployee(id);
+
+            if(employee != null)
+            {
+                _employeeData.DeleteEmployee(employee);
+                return Ok();
+            }
+
+            return NotFound($"Employee with ID: {id} was not found");
+        }
+
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditEmployee(Guid id,Employee employee)
+        {
+            var exsistingEmployee = _employeeData.GetEmployee(id);
+
+            if (exsistingEmployee != null)
+            {
+                employee.Id = exsistingEmployee.Id;
+                _employeeData.EditEmployee(employee);
+                
+            }
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,53 @@
+﻿using RestApiCRUD.Interface;
+using RestApiCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiCRUD.Services
+{
+    public class EmployeeService : IEmployeeData
+    {
+        private List<Employee> employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id=Guid.NewGuid(),
+                Name="Kamal"
+            },new Employee()
+            {
+                Id=Guid.NewGuid(),
+                Name="Namal"
+            },
+        };
+        public Employee AddEmployee(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            employees.Add(employee);
+            return employee;
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public Employee EditEmployee(Employee employee)
+        {
+            var exsistingEmployee = GetEmployee(employee.Id);
+            exsistingEmployee.Name = employee.Name;
+            return exsistingEmployee;
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
